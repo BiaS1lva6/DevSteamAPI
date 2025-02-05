f018406319a80f91a0b854c6fdf3d591bc33914e
Beatriz <beatriz080407@gmail.com>
2025-02-05 14:41:25 -0300

    inclusão dos endpoints do entity framework

--- DevSteamAPI/Program.cs
+++ DevSteamAPI/Program.cs
@@ -62,7 +62,9 @@ builder.Services.AddIdentityApiEndpoints<IdentityUser>(options =>
     options.SignIn.RequireConfirmedEmail = false;
     options.SignIn.RequireConfirmedAccount = false;
     options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireLowercase = false;
     options.Password.RequireUppercase = false;
+    options.Password.RequireDigit = false;
     options.Password.RequiredLength = 4;
 })
     .AddEntityFrameworkStores<DevSteamAPIContext>()
@@ -82,7 +84,7 @@ app.UseSwaggerUI();
 
 // Mapear os EndPoints padrão do Identyt FrameWork
 app.MapGroup("/Users").MapIdentityApi<IdentityUser>();
-app.MapGroup("/Roles").MapIdentityApi<IdentityRole>();
+//app.MapGroup("/Roles").MapIdentityApi<IdentityRole>();
 
 
 app.UseHttpsRedirection();
