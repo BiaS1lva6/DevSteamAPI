e0693059a30669e5ad7e45e4d0131e90002d620f
Beatriz <beatriz080407@gmail.com>
2025-02-11 10:07:05 -0300

    finalizado projeto de API Devsteam

---
+++ DevSteamAPI/Models/ItemCarrinho.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DevSteamAPI.Models
+{
+    public class ItemCarrinho
+    {
+        public Guid ItemCarrinhoId { get; set; }
+        public Guid CarrinhoId { get; set; }
+        public Guid JogoId { get; set; }
+
+        [Required(ErrorMessage = "O Campo é Obrigatório")]
+        [Range(1, int.MaxValue, ErrorMessage = "A Quantidade deve ser maior que 0")]
+        public int Quantidade { get; set; }
+
+        [Required(ErrorMessage = "O Campo é Obrigatório")]
+        [Range(0.01, 9999.99, ErrorMessage = "O Valor deve ser maior que 0 e menor que R$ 9.999,00")]
+        public decimal Valor { get; set; }
+        public decimal Total { get; set; }
+
+    }
+}
